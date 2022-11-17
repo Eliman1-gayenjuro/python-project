973f8222b2d2618ddd156e6c23f3b45764e54f32
Eliman1-gayenjuro <114258905+Eliman1-gayenjuro@users.noreply.github.com>
2022-11-17 15:10:20 +0000

    Add files via upload

---
+++ index.py
@@ -0,0 +1,657 @@
+# # # # # a =20
+# # # # # area = 6 * a *a
+# # # # # volume = a * a * a
+# # # # # print(area)
+# # # # # print (volume)
+
+
+
+# # # # # x = 20
+# # # # # x -= 3
+# # # # # print(x)
+
+# # # # # x = 55
+# # # # # z = 55
+# # # # # print(x)
+# # # # # print(z)
+
+
+# # # # # x = 5
+# # # # # y = 10
+
+# # # # # z = abs(x-y) * (x+y)
+# # # # # print(z)
+
+# # # # # import cmath
+# # # # # name = input(" Enter your name")
+# # # # # print(name)
+
+# # # # # number = int(input("Enter the value "))
+# # # # # print(type(number))
+
+
+# # # # # a =int(input("Enter the value of A: "))
+# # # # # b = int(input("Enter the value of B: "))
+# # # # # C = int(input("Enter the value of C: "))
+
+# # # # # d = (b**2) - (4 * a * C)
+# # # # # x_one = (-b + cmath.sqrt(d)) / 2 * a
+# # # # # x_two = (-b - cmath.sqrt(d)) / 2 * a
+
+
+# # # # # print(f'the value of x one{x_one }\n the value of {x_two} ') 
+
+
+# # # # # # x = 5
+# # # # # y = 11
+# # # # # z = x * y
+# # # # # print (z)
+
+# # # # # Name = "Eliman"
+# # # # # print(Name)
+# # # # # age = 24
+# # # # # print(age)
+# # # # # print (f'your name is(Eliman)\n your age is (24)')
+
+
+# # # # # import cmath
+# # # # # a_value = 7
+# # # # # b_value = 2
+# # # # # c_value = 3
+# # # # # d_value = (b_value **2) - (4 * a_value * c_value)
+# # # # # x_one = (-b_value + cmath.sqrt (d_value))/ 2*a_value
+# # # # # x_two = (-b_value - cmath.sqrt(d_value))/ 2* a_value
+# # # # # print(x_one)
+# # # # # print(x_two)
+
+
+# # # # # x = 20
+# # # # # if x % 2 ==0:
+# # # # #     print("even")
+# # # # # else:
+# # # # #     print("odd")
+    
+# # # # # import math
+# # # # # x = 2
+# # # # # y = x**2
+# # # # # z = math.sqrt(y)
+# # # # # print(y)
+# # # # # print(z)
+
+# # # # # from re import 
+
+# # # # # a = 20
+
+# # # # # area = 6 * a * a 
+# # # # # volume = a * a * a 
+# # # # # print(area)
+# # # # # print(volume)
+
+# # # # # x = 3
+# # # # # y = 5
+# # # # # a = 4
+
+# # # # # perimeter = x + y + a
+# # # # # print(perimeter)
+
+# # # # # print(f'the perimetr of the square is 12')
+ 
+
+# # # # # Nname = input('Eliman')
+# # # # # print(Name)
+
+# # # # # Name =input("Enter your name")
+# # # # # print(Name)
+# # # # # print (type(Name))
+
+
+# # # # # Number = input("Enter the value")
+# # # # # print(type(Number))
+
+# # # # # Name = int(input("Enter your name"))
+# # # # # print(type(Name))
+
+# # # # # import cmath
+
+# # # # # a = int(input("Enter the value of A:"))
+# # # # # b = int(input("Enter the value of B:"))
+# # # # # C = int(input("Enter the value of C:"))
+# # # # # d = (b**2)- (4*a* C)
+# # # # # X_one = (-b + cmath.sqrt(d))/2 * a 
+# # # # # x_two = (-b - cmath.sqrt(d))/2 * a
+
+# # # # # print(f'the value of x_one{X_one}\n the value of {x_two}')
+
+# # # # # my_age = 14
+# # # # # my_age += 3
+# # # # # # print(my_age)
+
+# # # # # days = 365
+# # # # # print(f'There are {days} in a year')
+
+
+# # # # # age = input("Enter your age:")
+# # # # # New_age = float(age)+ 40  
+# # # # # print(New_age)
+
+
+# # # # # km = 20
+
+# # # # # miles =  ( km * 0.621371)
+
+# # # # # print("%.2f km = %.2f miles"% (km , miles))
+
+# # # # # import calendar
+# # # # # Year= int(input (" Enter the year :"))
+# # # # # month = int(input("Enter the month:"))
+
+# # # # # My_Cal = calendar.month(Year,month)
+# # # # # print(My_Cal)
+
+# # # # # c = int(input("Enter the vcale in celcius: "))
+
+# # # # # d= 9/5 *c + 32
+
+# # # # # print(f'your vale in franhint is:{d} ')
+
+
+# # # # # import calendar
+
+# # # # # Year = int(input("Enter the Year:"))
+# # # # # # Month = int(input("Enter the Month:"))
+
+# # # # # # My_cal = calendar.month(Year,Month)
+
+# # # # # My_cal = calendar.calendar(Year)
+
+# # # # # print(My_cal)
+
+
+
+# # # # # mylist =  ['1','2','3','4','5']
+# # # # # print(mylist)
+# # # # # print(mylist[3])
+
+# # # # # mylisttwo =[ 'eliman','gaye']
+
+# # # # # mylist += mylisttwo
+
+# # # # # print(mylist)
+
+# # # # # mylist =  ['1','2','3','4','5']
+# # # # # # print(mylist[3])
+ 
+# # # # # # mylist =  ['1','2','3','4','5']
+
+# # # # # # print(mylist[1:3])
+
+# # # # # # mylist =  ['1','2','3','4','5']
+# # # # # # print(mylist)
+# # # # # # mylist.append(112)
+# # # # # # print(mylist)
+
+# # # # # mylist = ['Eliman', 'Ratoumatta', 'Rabiatou', 'Ahmed','Sundiata', 'Ala']
+# # # # # # # print(mylist)
+# # # # # # # mylist.append('Sundiata')
+# # # # # # mylist.insert(3, 'Awa')
+# # # # # # # mylist.remove('Sundiata')
+# # # # # mylist[1] = 'Adama'
+# # # # # print(mylist)
+
+
+# # # # # mylist = ['Eliman', 'Ratoumatta', 'Rabiatou', 'Ahmed','Sundiata']
+# # # # # if 'Eliman' in mylist:
+# # # # #     print('available')
+# # # # # else:
+# # # # #     print('not available')
+
+# # # # # import random
+
+# # # # # mydice = ['1','2','3','4','5','6']
+# # # # # game = random.choice(mydice)
+# # # # # print(game)
+
+
+# # # # # import random
+# # # # # friends = ['Bax','Ala']
+# # # # # Togohome = random.choice(friends)
+# # # # # print(Togohome)
+
+# # # # # names = ["Alieu", "Abubacar","Mahmood","jawo","Ebrima","Jainaba"]
+# # # # # # print(len(names))
+ 
+# # # # # # # # print(names[0])
+# # # # # # # # print(names[1])
+# # # # # print(names[-6])
+
+# # # # # names = ["sara","chanel","mitch","ryan","holy","alex","rob"]
+# # # # # # print(*names)
+# # # # # # print(names[1:4])
+# # # # # # print(names[0:3])
+# # # # # # print(names[4:7])
+# # # # # print(names[1::2])
+
+
+# # # # # my_list = ["Mitch",["sara", "sally", "joe"],"peter","aly"]
+# # # # # print(my_list[1][1:])
+# # # # # print(my_list[1])
+# # # # # print(my_list[3])
+# # # # # print(my_list[-1])
+
+# # # # # my_list = ["Mitch",["sara", "sally", "joe"],"peter","aly"]
+# # # # # # print(my_list)
+# # # # # # my_list.append("Eliman")
+# # # # # my_list.remove('peter')
+# # # # # print(my_list)
+
+# # # # # my_list[2] = 'Eliman'
+# # # # # print(my_list)
+
+# # # # # my_list.pop(2)
+# # # # # print(my_list)
+
+# # # # # my_list = [50,20,30,10,40,15,4]
+
+# # # # # my_list.sort(reverse = True)
+# # # # # print(my_list)
+
+# # # # # grocerylist = [['chips','jelly','chocolate'],['sweet potatoes','potatoes'],['peanuts','protein bar']]
+# # # # # print(grocerylist[0][2])
+# # # # # print(grocerylist)
+# # # # # print(grocerylist[1][1])
+# # # # # print(grocerylist[2][1])
+
+# # # # # TUPLE
+
+# # # # # my_list = ["mansa","kieta","jainaba","gaye"]
+# # # # # my_tuple =("mansa","kieta","jainaba","gaye")
+# # # # # print(my_tuple[-1])
+# # # # # print(len(my_tuple))
+# # # # # print(type(tuple))
+
+# # # # # my_tuple = ("Eliman",)
+# # # # # print(type(my_tuple))
+
+# # # # # my_tuple =("mansa","kieta","jainaba","gaye")
+# # # # # my_list = list(my_tuple)
+# # # # # my_list.append("Father")
+# # # # # my_tuple = tuple(my_list)
+# # # # # print(my_tuple)
+
+  
+# # # # # my_tuple = ("muhammed", "bax","bintou","suares","awa")
+# # # # # my_list = list(my_tuple)
+# # # # # my_list.remove("bax")
+# # # # # my_tuple = tuple(my_list)
+# # # # # print(my_tuple)
+
+
+# # # # # my_tuple = ("muhammed", "bax","bintou","suares","awa")
+# # # # # my_list = list(my_tuple)
+# # # # # my_list.pop(1)
+# # # # # my_tuple = tuple(my_list)
+# # # # # print(my_tuple)
+
+# # # # # my_tuple = ("muhammed", "bax","bintou","suares","awa")
+# # # # # (maize,rice,peanut,mango,orange) = my_tuple
+# # # # # print(maize)
+# # # # # print(mango)
+# # # # # second_tuple = ("maize","rice","peanut","mango","orange")
+# # # # # result = my_tuple + second_tuple
+# # # # # print(result)
+
+# # # # # my_tuple += second_tuple
+# # # # # print(my_tuple)
+
+# # # # # if "bintou" in my_tuple:
+# # # # #     print('available')
+# # # # # else:
+# # # # #     print('not available')
+    
+# # # # # for x in my_tuple:
+# # # # #     print(x)
+
+# # # # # my_detail = ("Eliman", 26)
+# # # # # print(f'Hello {my_detail}')
+
+# # # # # my_tuple = ("muhammed", "bax","bintou","suares","awa", "ous",)
+# # # # # # print(my_tuple)
+# # # # # print(my_tuple[-1])
+# # # # # print(my_tuple[0][1])
+
+
+# # # # # my_tuple = ('Eliman', "Gaye",25)
+# # # # # # print(len(my_tuple))
+# # # # # print(type(my_tuple))
+
+# # # # # my_tuple = ("hello",[87,40,60], "world",(10,25,30))
+# # # # # # print(my_tuple[1][1])
+# # # # # print(my_tuple[2])
+# # # # # # print(my_tuple[3][1])
+# # # # # # print(my_tuple)
+
+# # # # # my_tuple = (40, 3, 2, [10,4])
+# # # # # my_list=list(my_tuple)
+
+# # # # # my_tuple[2] = 10
+# # # # # my_tuple = tuple(my_list)
+# # # # # print(my_tuple)
+
+
+# # # # # my_tuple = ('math','science','python','english')
+# # # # # if 'python' in my_tuple:
+# # # # #     print('available')
+# # # # # else:
+# # # # #     print('not available')
+
+
+# # # # # Test_revision 
+
+# # # # # result = []
+# # # # # numbers = [ 23, 45, 66, 75, 54]
+# # # # # for x in numbers:
+# # # # #     result.append(x*x)
+# # # # #     print(result)
+
+
+
+# # # # # decimal = 10
+# # # # # result = oct(decimal)
+# # # # # print(result)
+
+
+# # # # # my_list = [23, 25,66,75,54]
+# # # # # my_list.reverse()
+# # # # # print(my_list)
+
+
+# # # # # SETS
+# # # # # sets are used to store multiple items in a single variable
+
+# # # # # my_set = {2,3,4,5,6}
+# # # # # # print(my_set)
+# # # # # # print(type(my_set))
+# # # # # # my_set.pop()
+# # # # # # my_set.remove(2)
+# # # # # # my_set.add("gaye")
+# # # # # set_of_class = {"jai", "fatima", "gaye","sow"}
+# # # # # # rest = my_set.union(set_of_class)
+# # # # # # my_set.update(set_of_class)
+# # # # # for i in my_set:
+# # # # #     print(i)
+# # # # # print(rest)
+# # # # # print(my_set)
+
+# # # # # set_of_class = {"jai", "fatima", "gaye","sow"}
+# # # # # if "fatima"  in set_of_class:
+# # # # #     print("YES")
+# # # # # else:
+# # # # # #     print("NO")
+
+# # # # # sample_set = {"yellow","orange","black"}
+# # # # # sample_list = ("blue","green","red")
+# # # # # # sample_set.update(sample_list)
+# # # # # res = sample_set.union(sample_list)
+# # # # # # print(sample_set)
+# # # # # print(res)
+
+# # # # # intersection_and_union
+# # # # # # set1 = {10,20,30,40,50}
+# # # # # # set2 = {30,40,50,60,70}
+# # # # # # res = set1.intersection(set2)
+# # # # # # # res = set1.union(set2)
+# # # # # # print(res)
+
+# # # # # # compliment
+# # # # # set1 = {10,20,30}
+# # # # # set2= {20,10,40,50}
+# # # # # # fad = set1.intersection(set2)
+# # # # # set1.difference_update(set2)
+# # # # # print(set1)
+# # # # # print(fad)
+
+
+# # # # # set1 = {10,20,30,40,50}
+# # # # # set1_to_remove =[10,20,30]
+# # # # # # set1.difference_update(set1_to_remove)
+# # # # # set1.difference_update([10,20,30])
+# # # # # print(set1)
+
+# # # # # friends = ["father","ala","bax","ajie"]
+# # # # # num = [20,30,40,60]
+# # # # # # num.append(3)
+# # # # # num.remove[30]
+# # # # # # friends.remove("ala")
+# # # # # print(num)
+
+# # # # # DICTIONARY
+
+# # # # # my_dictionary = {
+# # # # #                 "class":"jaina",
+# # # # #                 "teacher":"gaye",
+# # # # #                 "baller":"kieta"}
+# # # # # my_dictionary ["baller"]= "jallow"
+# # # # # print(my_dictionary)
+# # # # # print(type(my_dictionary))
+# # # # # # print(len(my_dictionary))
+# # # # # my_dictionary.pop("class")
+# # # # # print(my_dictionary)
+
+# # # # # gomindz = my_dictionary.copy()
+# # # # # print(gomindz)
+
+# # # # # x = {"a":"abie",
+# # # # #      "b":"abubacarr",
+# # # # #      "c":"ceesay"}
+# # # # # # for z in x:
+# # # # # #  print(z)
+# # # # # x.update({"lass": "laa"})
+
+# # # # # print(x)
+
+# # # # # car = {"brand":"ford",
+# # # # #        "model":"mustang",
+# # # # #        "year": 1964
+# # # #     #    }
+# # # # # print(car.get("model"))
+# # # # # print()
+# # # # # car["year"]=2020
+# # # # # car["color"]="red"
+# # # # # car.update({"color":"red"})
+# # # # # car.pop("model")
+# # # # # print(car)
+
+# # # # girl = "fatima"
+# # # # # boy = "eliman"
+# # # # # if girl == boy:
+# # # #   print("true")
+# # # # # else:
+# # # #     # print("false")
+
+# # # location = input("where are you going to:")
+# # # if location == "gomindz":
+# # #     print("are you going to attend class:")
+# # #     print("press 1 for python, press 2 for javaScript pres 3 for foundation")
+# # #     my_dic = {"python":"go to sundiatat's class",
+# # #             "javaScript":"go to ML class",
+# # #             "foundation":"go to konteh class"}
+    
+# # # for x in my_dic:
+# # #       print(x)
+# # #       school = input()
+# # #       if school == "python":
+          
+# # #          print("go to sundiata's class")
+# # #       elif school == "javaScript":
+# # #            print("go to ML class")
+# # # else:
+# # #     print("go home")
+
+# # # logical_statement
+
+# # # a = 33
+# # # b = 200
+# # # if b > a:
+# # #     print("b is greater than a")
+
+# # # a = 30
+# # # b = 30
+# # # if b < a:
+# #     # print(" b is greater than a")
+# # # elif a == b:
+# #     # print("a and b are equal")
+    
+# # # a = 200
+# # # b = 30
+# # # if b > a:
+# # #     print(" b is greater than  a")
+# # # elif a == b:
+# # #     print("a and b are equal")
+# # # else:
+# # #     print("a is greater than b")
+    
+
+# # # a = 33
+# # # b = 200
+# # # if b > a:
+# # #     print(" b is greater than a")
+# # # else:
+# # #     print("b is not greater than a")
+
+# # # a = 2
+# # # b = 330
+
+# # # print("A") if a > b else print("B")
+
+# # # a = 50
+# # # b = 10
+# # # if a > b:
+# # #     print("Hello world")
+
+# # # a = 5
+# # # b = 2
+# # # print("Yes") if a > b else print("No")
+
+# # # print("YES") if 5>2 else print("NO")
+    
+
+# # # Try_me
+
+# # # print('python'=='PYTHON')
+# # # print('PYTHON'=='PYTHON')
+# # # print('PYTHON'!='PYTHON')
+# # # print(15!=10)
+
+# # # print('Hello'!='Hello' and (6>2) and (5==5))
+# # # print('Hello'!='HELLO' or (6>2)or (5==5))
+# # # 
+# # # x = 20
+# # # y = 30
+# # # print(x==y)
+# # # print(x!=y)
+# # # print(x> y)
+# # # print(x<y)
+# # # print(x==y or x!=y)
+# # # # print(x==y and x!=y)
+
+# # # input_dictionary = {"Key 1": 20,
+# # #                     "key 2": 30,
+# # #                     "key 3": 50}
+# # # out_dictionary = []
+# # # if input_dictionary["key 1"] >=30:
+# # #    out_dictionary.append(input_dictionary["key 2"])
+# # # if input_dictionary["key 2"] >= 30:
+# # #     out_dictionary.append(input_dictionary["key 2"])
+# # # print(out_dictionary)
+
+
+# # # import random
+# # # my_list=[1,2,3,4,24,6,10]
+# # # result = random.choice(my_list)
+# # # print(result)
+
+# # import random
+# # result = random.randint(1,100)
+
+# # user= int(input("Enter your value:"))
+# # if result>user:
+# #     print('computer has a greater choice', result)
+# # elif result < user:
+# #     print('computer has a smaller choice',result)
+# # else:
+# #     print('you have the same choice', result)
+
+
+# # # num = float(input("input a number"))
+# # # if num>0:
+# # #     print("its positive number")
+# # # else:
+# # #     print (" its a negative number")
+
+
+# #
+# # num = float(input("Enter any value:"))
+# # if num > 0:
+# #     if num ==0:
+# #         print("zero")
+# #     else:
+# #         print("positive")
+# # else:
+#         # print("negative")
+    
+
+# # number 7
+# numb = float(input("Enter any number:"))
+# if numb %5 ==0 and numb % 7 != 0:
+    
+#     print("number is divisible by 5")
+# else:
+#     print("number is not a multiple")
+
+
+# num = input("Enter a value:")
+# num =[2,3,4,5,6]
+# print(len(num))
+
+
+
+# # num 9
+# # num = int(input("Enter any number to test whether it is odd or even:"))
+# # if (num % 2)==0:
+# #     print("The number is even")
+# # else:
+# #     print("The provided number is odd")
+
+# my_dic = {"sally": 23,
+        #   "dina": 30,
+        #   "holy": 50}
+#
+    
+
+
+# def check_fermat(a, b, c, n):
+        # if n > 2 and (a**n + b**n==c**n):
+                # print("Holly smokes, fermat was wrong!")
+        # else:
+                # print("No, that doesn't work." )
+# def check_numbers():
+#         a = int(input("choose a number for a: "))
+#         b = int(input("choose a number for b: "))
+#         c = int(input("choose a number for c: "))
+#         # n = int(input("choose a number for n: "))
+#         return check_fermat (a, b, c, n)
+# check_numbers()
+
+
+
+password = 3811959
+wrong_pasword_store = 5
+
+while wrong_pasword_store != 0:
+        ask_password = int(input("Enter your password:"))
+        
+        if ask_password != password:
+            wrong_pasword_store -= 1
+            print(f'you are left with {wrong_pasword_store} trials')
+        else:
+            print("You have login")
